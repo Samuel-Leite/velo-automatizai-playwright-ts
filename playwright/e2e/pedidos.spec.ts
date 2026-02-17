63e4726054b29378f5204c2bb070bd98c87ca060
Samuel Leite <samuel-leite@outlook.com.br>
2026-02-17 11:47:57 -0300

    inclusao cenario em analise

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -115,6 +115,56 @@ test.describe('Consulta de pedido', async () => {
             `);
     });
 
+    test('Deve consultar um pedido em analise', async ({ page }) => {
+
+        // Test Data
+        const order = {
+            number: 'VLO-BRDRYI',
+            status: 'EM_ANALISE',
+            color: 'Lunar White',
+            wheels: 'aero Wheels',
+            customer: {
+                name: 'Donald McDuck',
+                email: 'donald@gmail.com'
+            },
+            payment: 'À Vista'
+        }
+
+        // Act
+        await page.getByTestId('search-order-id').fill(order.number)
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+        // Assert
+        await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order.number}
+            - img
+            - text: ${order.status}
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph:  ${order.color}
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: ${order.wheels}
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: ${order.customer.name}
+            - paragraph: Email
+            - paragraph: ${order.customer.email}
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: ${order.payment}
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+    });
+
     test('Deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
 
         const order = generateOrderCode()
