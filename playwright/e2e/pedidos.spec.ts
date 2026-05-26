ffed99ad2c5d13ef379a4f23ee83484841b1a656
Samuel Leite <samuel-leite@outlook.com.br>
2026-05-26 20:48:14 -0300

    Organizando locators com Elements Laye

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '../support/fixtures'
+import { test, expect } from '../support/fixtures'
 import { generateOrderCode } from '../support/helpers'
 
 test.describe('Consulta de Pedido', () => {
@@ -71,5 +71,13 @@ test.describe('Consulta de Pedido', () => {
     const orderCode = 'XYZ-999-INVALIDO'
     await app.orderLockup.searchOrder(orderCode)
     await app.orderLockup.validateOrderNotFound()
+  })
+
+  test('Deve manter o botão de busca desabilitado com campo vazio ou apenas espaços', async ({ app }) => {
+    const button = app.orderLockup.elements.searchButton
+    await expect(button).toBeDisabled()
+
+    await app.orderLockup.elements.orderInput.fill('     ')
+    await expect(button).toBeDisabled()
   })
 })
