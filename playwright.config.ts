b4cfca6299ae989fd01ee8294cc5860f2e573e88
Samuel Leite <samuel-leite@outlook.com.br>
2026-06-09 20:03:03 -0300

    adicionando build deploy no github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-playwright.vercel.app/',
+    baseURL: 'https://velo-automatizai-playwright-6hxypcsv0-samuel-leite1.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
