8b9ca5ce150c11eb673b687bf609265942ad91f2
Samuel Leite <samuel-leite@outlook.com.br>
2026-06-18 21:40:48 -0300

    correcao variaveis preview 11v

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-if (!process.env.VERCEL) {
-  dotenv.config({ path: path.resolve(__dirname, '.env') });
-}
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
