c4a078dfd4b709fc65bc186af3bc124c29490cc8
Samuel Leite <samuel-leite@outlook.com.br>
2026-07-14 20:21:54 -0300

    correcoes base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-automatizai-playwright-3a30cl2k6-samuel-leite1.vercel.app/',
+    baseURL: 'https://velo-playwright.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
